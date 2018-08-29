7e290f5166d98934174f2473876c25ac00d0f1fe
devinkapelanski@yahoo.com <devinkapelanski@yahoo.com>
2018-08-29 18:19:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ if (name === "Joe") {
   console.log("The name is correct")
 }
 if (height === "74") {
-  console.log(The height is correct)
+  console.log("The height is correct")
 }
 // Don't worry about this
 module.exports = { name, height
